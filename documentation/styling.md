10122f7a9566f1d59dc762e18bc52cb8d1e46fd3
andres Castro <andres Castro>
2023-04-21 11:42:45 -0600

    menu Ready

---
+++ documentation/styling.md
@@ -0,0 +1,10 @@
+# Styles
+All the Icons for this web site were integrated from the library FontAwesome. Please visit https://fontawesome.com/ for more information about this lirbary. 
+
+
+## FontAwesome account
+Username: mckaymaclab 
+Password: Maclabcode140a 
+
+The Font Awesome Kit connection link  
+<script src="https://kit.fontawesome.com/74dc4d0f8d.js" crossorigin="anonymous"></script>
--- js/loadMenu.js
+++ js/loadMenu.js
@@ -6,16 +6,26 @@ class menu  extends HTMLElement{
   connectedCallback(){
     this.innerHTML =`
     <nav>
-      <div class="nav_logo-container">
-            <div class="logo-container_left"></div>
-            <div class="logo-container_right"></div>
-      </div>
-      <ul class="nav_buttons-container" >
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>         
-      </ul>
-    </nav>`
+    <div class="nav_logo-container">
+          <div class="logo-container_left"></div>
+          <div class="logo-container_right"></div>
+    </div>
+    <ul class="nav_buttons-container" >
+        <li>
+            <a>Services <i class="fa-solid fa-angle-down"></i></a>
+            <ul class="dropdown-content">
+              <li><a>3D Priting</a></li>
+              <li><a>Adobe Help</a></li>
+              <li><a>Equipement Check Out</a></li>
+              <li><a href="/pages/vr.html">Vr Lab</a></li>
+              <li><a>Workshops</a></li>
+              <li><a>Cricut</a></li>
+            </ul>
+          </li>
+          <li><a href="">Events</a></li>
+          <li><a href="">Contact Us</a></li>         
+    </ul>
+  </nav>`
   }
 }
 
