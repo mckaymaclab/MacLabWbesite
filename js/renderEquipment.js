d6135a7a7396fd40f222807b998d758b224d00aa
andres Castro <andres Castro>
2023-06-07 01:49:57 -0600

    Equipment test

---
+++ js/renderEquipment.js
@@ -0,0 +1,27 @@
+const getData = async () =>{
+    const res = await fetch('../js/equipment.json');
+    const data = await res.json();
+    return data;
+} 
+
+const displayEquipment = async () =>{ 
+    const equipmentInfo = await getData()
+    equipmentInfo.forEach(item => {
+        const card =`
+        <div class="equipmentCard">
+            <div>
+                <h2>${item.title}<h2>
+                <h3>${item.RentalPeriod}<h3>
+                <p>${item.description}</p>
+            </div>
+            <div>
+            <img src="${item.Picture}"><img>
+            </div>
+        </div>
+        `  
+        document.querySelector('section').insertAdjacentHTML('beforeend',card)
+    });  
+}
+
+displayEquipment();
+    
