fbc9aaec89c063f9393ba15bde53ec1fe6ffea2a
tjlowry <low22022@byui.edu>
2023-07-06 09:15:58 -0600

    worked more on vr scroll section

---
+++ js/vrscroll.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.section_div-appsContainer');
+    const leftArrow = document.querySelector('.left-arrow');
+    const rightArrow = document.querySelector('.right-arrow');
+  
+    leftArrow.addEventListener('click', function() {
+      container.scrollBy({
+        left: -750, // Adjust the scroll amount as needed
+        behavior: 'smooth'
+      });
+    });
+  
+    rightArrow.addEventListener('click', function() {
+      container.scrollBy({
+        left: 750, // Adjust the scroll amount as needed
+        behavior: 'smooth'
+      });
+    });
+  });
+  
